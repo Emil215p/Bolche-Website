c57f542f741d3507e61f5fbd13c101cfeba25aec
Emil215p <juelemil@gmail.com>
2024-07-01 20:22:13 +0200

    Color system works.

--- BolcheWebsite/wwwroot/js/site.js
+++ BolcheWebsite/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿let currentSearchMode = 'none'; // Search mode is set to none by default.
+﻿console.log(bolcheData);
+
+let currentSearchMode = 'none'; // Search mode is set to none by default.
 function toggleON() {
     document.getElementById("BolcheTabel").style.display = "block";
 }
@@ -18,7 +20,28 @@ function SearchForGeneralShift() {
 }
 
 function SetColor() {
-    alert("Not implemented yet.");
+    document.getElementById("BolcheTabel").style.display = "block";
+    const input = document.getElementById("colors");
+    const selectedColor = input.value;
+
+    const table = document.getElementById("BolcheTabel");
+    const tr = table.getElementsByTagName("tr");
+
+    for (let i = 0; i < tr.length; i++) {
+        const tdColor = tr[i].getElementsByTagName("td")[1];
+        const tdName = tr[i].getElementsByTagName("td")[0];
+
+        if (tdColor && tdName) {
+            const rowColor = tdColor.textContent || tdColor.innerText;
+            const rowName = tdName.textContent || tdName.innerText;
+
+            if (rowColor === selectedColor || rowName.toUpperCase().includes(input.value.toUpperCase())) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
 }
 
 function SearchThing() {
@@ -52,4 +75,27 @@ function SearchThing() {
     if (currentSearchMode === 'none') {
         alert("Vælg søgekriterier eller klik på Vis alle.");
     }
+}
+
+const uniqueColors = {};
+
+// Filter out non-unique colors
+const filteredArray = bolcheData.filter((bolche) => {
+    const color = bolche.farve;
+    if (!uniqueColors[color]) {
+        uniqueColors[color] = true;
+        return true; // Include the first occurrence of each color
     }
+    return false; // Exclude subsequent occurrences
+});
+
+function populateColorOptions() {
+    const select = document.getElementById("colors");
+    filteredArray.forEach((bolche) => {
+        const option = document.createElement("option");
+        option.value = bolche.farve;
+        option.text = bolche.farve;
+        select.appendChild(option);
+    });
+}
+populateColorOptions();
